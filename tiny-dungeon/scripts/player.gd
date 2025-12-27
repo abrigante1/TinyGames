6cb351a97b5810b263243c19f5d10b24843cca0c
Anthony Avancena-Brigante <anthonypbrigante@gmail.com>
2025-12-27 17:35:07 -0500

    Initial Scripts

---
+++ tiny-dungeon/scripts/player.gd
@@ -0,0 +1,25 @@
+class_name Player
+extends CharacterBody2D
+
+
+const SPEED = 32.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var x_direction := Input.get_axis("left", "right")
+	if x_direction:
+		velocity.x = x_direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	var y_direction := Input.get_axis("up", "down")
+	if y_direction:
+		velocity.y = y_direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
